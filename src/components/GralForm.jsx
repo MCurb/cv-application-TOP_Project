ace9cd71b5141a1a1a8c8d5c110a5cfd0c23f67f
MCurb <curbecomarcos.dev@gmail.com>
2026-05-19 09:49:34 -0600

    feat: add GralForm component

---
+++ src/components/GralForm.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { Button } from "./Button";
+import { Input } from "./Input";
+
+export function GralForm({ onFormSubmit }) {
+  const [gralInfoInputValues, setGralInfoInputValues] = useState({
+    fullName: "",
+    email: "",
+    number: "",
+  });
+
+  function onInputChange(e) {
+    const { name, value } = e.target;
+    setGralInfoInputValues({ ...gralInfoInputValues, [name]: value });
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+    onFormSubmit(gralInfoInputValues)
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <fieldset>
+        <legend>General Information:</legend>
+        <Input
+          label={"Full Name:"}
+          placeholder={"Marcos Curbeco"}
+          id={"input-name"}
+          name={"fullName"}
+          onChange={onInputChange}
+          required
+        ></Input>
+        <Input
+          label={"Email:"}
+          placeholder={"youremail@gmail.com"}
+          id={"input-email"}
+          name={"email"}
+          onChange={onInputChange}
+          type="email"
+          required={gralInfoInputValues.number === "" && true}
+        ></Input>
+        <Input
+          label={"Phone Number:"}
+          placeholder={"548 654 1526"}
+          id={"input-number"}
+          name={"number"}
+          onChange={onInputChange}
+          type="number"
+          required={gralInfoInputValues.email === "" && true}
+        ></Input>
+      </fieldset>
+      <div>
+        <Button type="submit" text={"Submit"}></Button>
+        <Button text={"Clear"}></Button>
+      </div>
+    </form>
+  );
+}
